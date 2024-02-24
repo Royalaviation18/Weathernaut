2fda4a3137068ae98eabb3d90573d69a76667ba3
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2024-02-24 22:19:44 +0530

    Home Screen UI Update 1.3

--- app/src/main/java/com/example/weathernaut/SplashActivity.kt
+++ app/src/main/java/com/example/weathernaut/SplashActivity.kt
@@ -5,15 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.os.Handler
+
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this,HomeActivity::class.java)
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
-        },4000)
+        }, 4000)
     }
 }
